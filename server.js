327064b7bf3a86bb35a3363af3772d9b9f82da60
ArpitCS <arpit2005.garg@gmail.com>
2025-04-08 09:41:21 +0530

    Directory Setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
