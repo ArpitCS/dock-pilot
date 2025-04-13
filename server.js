73f0a789707a358eacd72819839223606ed3966d
ArpitCS <arpit2005.garg@gmail.com>
2025-04-13 15:51:39 +0530

    Folder Reset

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "public")));
-
 app.listen(3000, () => {
   console.log("http://localhost:3000");
 });
